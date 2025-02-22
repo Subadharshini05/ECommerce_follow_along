9d8c354edd5b5732e79a8e89973370f49d182848
suba dharshini <subakarthick2005@gmail.com>
2025-02-22 10:05:08 +0530

    done

---
+++ frontend/src/pages/cart.jsx
@@ -0,0 +1,15 @@
+import NavBar from '../components/Navbar';
+import React from '../components/Navbar';
+
+const cart = () => {
+  return (
+    <>
+       <NavBar/>
+       <div>
+         <h1>Cart</h1>
+       </div>
+    </>
+     );
+}
+
+export default cart
